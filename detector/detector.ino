c4ff6be6ead2a62d6f01739632a036ce0075ce83
Tom <treez.zhang@gmail.com>
2022-02-11 23:41:39 -0800

    add read

--- detector/detector.ino
+++ detector/detector.ino
@@ -11,9 +11,37 @@ const int d7 = 2;
 
 const int led = 6;
 
+// Dimensions of the probe
+// In mm
+const int radius = 5;
+const int heihgt = 5;
+
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 int cond; // Conductivity value
+int rawRead; // Raw Readings
+float voltage; // Voltage after calculation
+
+int condCalc(int v){
+  
+  
+}
+
+double consCalc(int cond){
+
+}
+
+void updateLCD(int cond, double cons) {
+ lcd.setCursor(0,0);
+ lcd.print("Conductivity: ");
+ lcd.print(cond);
+
+ lcd.setCursor(0,1);
+ lcd.print("Consentration: ");
+ lcd.print(cons);
+ lcd.print(" ppm");
+  
+}
 
 void setup() {
   pinMode(led, OUTPUT);
@@ -21,5 +49,11 @@ void setup() {
 }
 
 void loop() {
+  rawRead = analogRead(A0);
   
+  if (rawRead != 0) {digitalWrite(led, HIGH);}
+  else {digitalWrite(led, LOW);}
+
+  voltage = rawRead*(5.0/1023.0);
+
 }
